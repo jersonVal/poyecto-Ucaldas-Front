85b09a43129cf734f980ffcb2316362031ebef2e
jersonval96 <jersonDevelopment@gmail.com>
2021-11-21 13:21:07 -0500

    new changes dropdown

--- src/assets/js/init-M.js
+++ src/assets/js/init-M.js
@@ -1,11 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
+    var elemsSide = document.querySelectorAll('.sidenav');
+    var elemsDrop1 = document.querySelectorAll('.dropdown-trigger');
+    var elemsDrop2 = document.querySelectorAll('.dropdown-trigger-2');
+
+    var sidenav = M.Sidenav.init(elemsSide, {});
+
+    var drop1 = M.Dropdown.init(elemsDrop1, {
+        // closeOnClick: false,
+        coverTrigger: false
+    });
+
+    var instance = M.Dropdown.getInstance(elemsDrop1);
+    console.log(drop1);
+
+    var drop2 = M.Dropdown.init(elemsDrop2, {
+        // closeOnClick: false
+        hover: true,
+        alignment: 'right',
+        onCloseEnd: () => {
+            console.log(instance)
+        }
+    });
+
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, {
-        hover: true
-    });
-});
+// let instance = {}
+
+// document.addEventListener('DOMContentLoaded', function () {
+
+    
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+
+    
+// });
