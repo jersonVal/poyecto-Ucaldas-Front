19dfea97b8442e0fe57f0602898e1f8fb233e8af
jersonval96 <jersonDevelopment@gmail.com>
2021-11-21 12:03:31 -0500

    dropdown menu

--- src/app/modules/reportes/reportes-routing.module.ts
+++ src/app/modules/reportes/reportes-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JuradosDeSolicitudComponent } from './jurados-de-solicitud/jurados-de-solicitud.component';
+import { LineaDeInvestigacionDeJuradoComponent } from './linea-de-investigacion-de-jurado/linea-de-investigacion-de-jurado.component';
+import { ProponenteDeSolicitudComponent } from './proponente-de-solicitud/proponente-de-solicitud.component';
+import { ResultadoEvaluacionComponent } from './resultado-evaluacion/resultado-evaluacion.component';
+import { TipoDeComiteDeSolicitudComponent } from './tipo-de-comite-de-solicitud/tipo-de-comite-de-solicitud.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "jurados-de-solicitud",
+    component: JuradosDeSolicitudComponent
+  },
+  {
+    path:"lineas-de-investigacion-de-jurado",
+    component: LineaDeInvestigacionDeJuradoComponent
+  },
+  {
+    path: "proponente-de-solicitud",
+    component: ProponenteDeSolicitudComponent
+  },
+  {
+    path: "resultado-evaluacion",
+    component: ResultadoEvaluacionComponent
+  },
+  {
+    path: "tipo-comite-de-solicitud",
+    component: TipoDeComiteDeSolicitudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/public/template/navbar/navbar.component.ts
+++ src/app/public/template/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  session: boolean = false;
+  session: boolean = true;
 
   constructor() { }
 
