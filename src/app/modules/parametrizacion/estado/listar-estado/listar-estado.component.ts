b1cc4e9b872a65e4e4616bf12a45bb41ca59f08a
jersonval96 <jersonDevelopment@gmail.com>
2021-12-08 12:07:03 -0500

    color trash

--- src/app/modules/parametrizacion/estado/listar-estado/listar-estado.component.ts
+++ src/app/modules/parametrizacion/estado/listar-estado/listar-estado.component.ts
@@ -22,16 +22,10 @@ export class ListarEstadoComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-<<<<<<< HEAD
     // this.subscripcion = 
     this.GetRecord();
   }
 
-=======
-    this.GetRecord()
-  }
-  
->>>>>>> 62fff0fd65c7393dddd57bc0813594e573a0bccf
   GetRecord(){
     this.estadoService.getRecord().subscribe(
       {
